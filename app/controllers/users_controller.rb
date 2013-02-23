4858395927d424a21452e686b29dcb0f0bba6beb
kai <lovefamilydev@gmail.com>
2013-02-23 23:13:50 +0900

    vote

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -84,15 +84,15 @@ class UsersController < ApiController
       return      
     end
 
-    if (@user.voted_down_on? @alert_user)
-      render :json=>{:success => false, :result_code => 1, :message=>"already alert user"}
-      return
-    end
+    # if (@user.voted_down_on? @alert_user)
+    #   render :json=>{:success => false, :result_code => 1, :message=>"already alert user"}
+    #   return
+    # end
 
-    #if (@alert_user.downvote_from @user)
-      #newCount = @alert_user.downvotes.size
-    if (@user.down_votes @alert_user)
-      render :json=>{:success => true, :result_code => 0, :message=>"success to update alert count. current alert"}
+    if (@alert_user.downvote_from @user)
+      newCount = @alert_user.downvotes.size
+    # if (@user.down_votes @alert_user)
+      render :json=>{:success => true, :alert_count=> newCount, :message=>"success to update alert count. current alert #{newCount}"}
     else
       render :json=>{:success => false, :result_code => 2, :message=>"fail to update alert count."}
     end
