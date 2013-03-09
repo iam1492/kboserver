3701b33a2fce57e3bd9a641ab8672f0edc4cb3ff
kai <lovefamilydev@gmail.com>
2013-03-09 09:56:29 +0900

    alert modify

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,8 +1,8 @@
 class Relationship < ActiveRecord::Base
   attr_accessible :alerted_id
 
-  belongs_to :alerter, :class_name => "User"#, :counter_cache => true
-  belongs_to :alerted, :class_name => "User"
+  belongs_to :alerter, :class_name => "User"
+  belongs_to :alerted, :class_name => "User", :counter_cache => :alerters_count
 
   validates :alerter_id, presence: true
   validates :alerted_id, presence: true
