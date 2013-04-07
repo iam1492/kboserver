0023619e467463079ee0265671899b38ba3f318b
kai <lovefamilydev@gmail.com>
2013-04-07 10:52:56 +0900

    profile ratio

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :blocked, :imei, :nick_count, :nickname, :user_type, :alerter_count, :alerters_count, :profile, :intro, :updated_at
-  has_attached_file :profile, :styles => { :original => "400x400>", :medium => "200x200>", :thumb => "100x100>" }
+  has_attached_file :profile, :styles => { :original => "720x", :medium => "200x200>", :thumb => "100x100>" }
 
   after_save    :expire_user_cache
   after_destroy :expire_user_cache
