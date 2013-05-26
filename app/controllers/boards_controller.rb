8f058c605e1ec665416a64cc5acbdee5b246ca85
kai <lovefamilydev@gmail.com>
2013-05-27 00:45:50 +0900

    modify error2

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -40,12 +40,12 @@ class BoardsController < ApiController
 		end
 
 		@requestUser = User.getUserInfo(@imei);
-		if (@requestUser.nil?  || !(@board.imei.eql? "ekseo00"))
+		if (@requestUser.nil?  || !(@imei.eql? "ekseo00"))
 			render :json=>{:success => false, :result_code => 2, :message=>"no user found"}
 			return
 		end
 
-		if (@board.imei.eql? @imei || (@board.imei.eql? "ekseo00"))
+		if (@board.imei.eql? @imei || (@imei.eql? "ekseo00"))
 			if(@board.destroy)
 				render :json=>{:success => true, :result_code => 0, :message=>"success to delete articles."}
 			else
